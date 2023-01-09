cff1354d29139a21a061a0f7b025d37b3dcb475b
JAEMOON <jaemoonzzang@naver.com>
2023-01-09 23:46:57 +0900

    app.js 수정

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,25 +1,50 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
 
+/**
+ * @description 객체 타입을 미리 정의한다면 브라우저에서 값을 살펴보지 않고도 code 상에서 오류 도출 가능
+ * @typedef {object} address
+ * @property {string} city
+ * @property {string} street
+ *
+ */
+
+/**
+ * @description 프로퍼티의 타입 정의 User 객체 > console.log 로 찍어보지 않고도 바로보기로 나타나게 됨
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+    return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
-    .then(function (response) {
-      console.log(response);
-      user = response.data;
-      // TODO: 이름, 이메일, 주소 표시하기
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    fetchUser
+        .then(function (response) {
+            //console.log(response);
+            user = response.data;
+
+            username.innerText = user.name;
+            email.innerText = user.email;
+            address.innerText = user.address.street;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 startApp();
