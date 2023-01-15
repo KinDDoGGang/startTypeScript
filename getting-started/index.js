5be2af00b23f64ac922f93e65017eddd2144db9c
JAEMOON <jaemoonzzang@naver.com>
2023-01-16 00:05:14 +0900

    index.ts -> index.js ( 컴파일 )

---
+++ getting-started/index.js
@@ -0,0 +1,5 @@
+// 파라미터: 타입, 파라미터b: 타입, 리턴: 타입
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
---
+++ getting-started/index.ts
@@ -0,0 +1,6 @@
+// 파라미터: 타입, 파라미터b: 타입, 리턴: 타입
+function sum(a : number,b : number) : number {
+    return a+ b;
+}
+
+sum(10,20);
